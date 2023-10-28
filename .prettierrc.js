fe4bfa791bbd1be6461d2e6d4e7364aeefb9cd30
kimulaco <kimulaco@gmail.com>
2023-10-28 20:33:35 +0900

    Add eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ['prettier-plugin-astro'],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+}
+
+export default config
