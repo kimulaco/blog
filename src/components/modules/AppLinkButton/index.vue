f60f3d3132cfd740cbcf90d115a8f153a94e2059
kimulaco <kimulaco@gmail.com>
2023-11-02 07:14:41 +0900

    Add 404 page

---
+++ src/components/modules/AppLinkButton/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+type Props = {
+  href: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <a :href="href" class="LinkButton">
+    <slot />
+  </a>
+</template>
+
+<style scoped lang="scss">
+@use '../../../assets/scss/variables' as variables;
+
+.LinkButton {
+  display: inline-block;
+  color: variables.$COLOR_WHITE;
+  background: variables.$COLOR_BLACK;
+  font-size: 15px;
+  text-align: center;
+  min-width: 160px;
+  padding: 8px 12px;
+  margin: 0;
+  border-radius: 4px;
+}
+</style>
---
+++ src/components/modules/PageTitle/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="PageTitle"><slot /></h1>
+</template>
+
+<style scoped lang="scss">
+.PageTitle {
+  font-size: 32px;
+  margin: 0 0 32px;
+}
+</style>
