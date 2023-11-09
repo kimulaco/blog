c8c5db9324640028875fa11813e9e52354a528e2
kimulaco <kimulaco@gmail.com>
2023-11-10 07:31:57 +0900

    Add GA4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
 import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
 import svgLoader from 'vite-svg-loader'
 import { APP_CONFIG } from './config'
 
 export default defineConfig({
   site: APP_CONFIG.URL.ORIGIN,
-  integrations: [vue(), sitemap()],
+  integrations: [
+    vue(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   vite: {
     plugins: [svgLoader()],
   },
