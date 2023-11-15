27ef2d21498fc70214e7d7826e56946cbadb6900
kimulaco <kimulaco@gmail.com>
2023-11-15 22:08:06 +0900

    Add JSON-LD

---
+++ src/core/domains/breadcrumb/createBreadcrumbList.ts
@@ -0,0 +1,21 @@
+import { APP_CONFIG } from '@@/config/app'
+import type { Breadcrumb } from './types'
+
+export const createBreadcrumbList = (
+  breadcrumbs: Breadcrumb[]
+): Breadcrumb[] => {
+  return [
+    {
+      url: APP_CONFIG.URL.ORIGIN,
+      text: 'TOPページ',
+    },
+    ...breadcrumbs.map((breadcrumb) => {
+      return {
+        url: /^http(|s):\/\//.test(breadcrumb.url)
+          ? breadcrumb.url
+          : `${APP_CONFIG.URL.ORIGIN}${breadcrumb.url}`,
+        text: breadcrumb.text,
+      }
+    }),
+  ]
+}
