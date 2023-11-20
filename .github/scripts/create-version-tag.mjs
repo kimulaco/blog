286b2ec5eba956afd20ce8cc2b7982a74bb1dfa9
kimulaco <kimulaco@gmail.com>
2023-11-20 23:06:08 +0900

    Add description to release note

--- .github/scripts/create-version-tag.mjs
+++ .github/scripts/create-version-tag.mjs
@@ -4,4 +4,9 @@ import { $ } from 'zx'
 const developPackage = JSON.parse(await $`cat package.json`)
 const developVersion = developPackage.version
 
-await $`gh release create ${developVersion} -t ${developVersion}`
+const prs =
+  await $`gh pr list --repo ${$GITHUB_REPOSITORY} --search "'${developVersion}' in:title" -s all --json url`
+
+const description = prs.map((pr) => `- (${pr.url})`).join('\n')
+
+await $`gh release create ${developVersion} -t ${developVersion} -n "${description}" --latest`
