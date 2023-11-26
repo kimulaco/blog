5f3229ac08d89d11f8021e4ac99dba332d4efe64
kimulaco <kimulaco@gmail.com>
2023-11-26 17:52:34 +0900

    Fix zx env

--- .github/scripts/create-version-tag.mjs
+++ .github/scripts/create-version-tag.mjs
@@ -5,7 +5,7 @@ const developPackage = JSON.parse(await $`cat package.json`)
 const developVersion = developPackage.version
 
 const prs =
-  await $`gh pr list --repo ${$GITHUB_REPOSITORY} --search "'${developVersion}' in:title" -s all --json url`
+  await $`gh pr list --repo $GITHUB_REPOSITORY --search "'${developVersion}' in:title" -s all --json url`
 
 const description = prs.map((pr) => `- (${pr.url})`).join('\n')
 
