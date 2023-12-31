aa8c5f615fd5b3320d7ba94eec2216a6e88c4850
kimulaco <kimulaco@gmail.com>
2023-12-31 22:59:21 +0900

    Fix release note

--- .github/scripts/create-version-tag.mjs
+++ .github/scripts/create-version-tag.mjs
@@ -8,6 +8,6 @@ const prs = JSON.parse(
   await $`gh pr list --repo $GITHUB_REPOSITORY --search "'${developVersion}' in:title" -s all --json url`
 )
 
-const description = prs.map((pr) => `- (${pr.url})`).join('\n')
+const description = prs.map((pr) => `- ${pr.url}`).join('\n')
 
 await $`gh release create ${developVersion} -t ${developVersion} -n "${description}" --latest`
