2d925e99058dd3cafdd401e1bf30cd624881ace6
kimulaco <kimulaco@gmail.com>
2024-11-04 00:41:12 +0900

    Update stylelint to v16.10

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,14 +1,26 @@
-module.exports = {
+/** @type {import('stylelint').Config} */
+const config = {
   extends: [
     // TODO: Enable prettier
     // 'stylelint-prettier/recommended',
     'stylelint-config-standard-scss',
   ],
   plugins: ['stylelint-scss', 'stylelint-prettier'],
-  customSyntax: 'postcss-html',
   rules: {
+    'color-named': ['never'],
     'selector-class-pattern': null,
+    'rule-empty-line-before': null,
     'scss/at-mixin-argumentless-call-parentheses': 'always',
     'scss/at-import-no-partial-leading-underscore': null, // deprecated
+    'scss/dollar-variable-empty-line-before': null,
+    'scss/dollar-variable-pattern': null,
   },
+  overrides: [
+    {
+      files: ['**/*.astro'],
+      extends: ['stylelint-config-html/astro'],
+    },
+  ],
 }
+
+export default config
