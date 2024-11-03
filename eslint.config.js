939f24933062a5591a93758d44a7753c8a007094
kimulaco <kimulaco@gmail.com>
2024-11-04 00:02:19 +0900

    Update eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import typescriptEslintParser from '@typescript-eslint/parser'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import eslintPluginAstro from 'eslint-plugin-astro'
+import globals from 'globals'
+
+export default [
+  {
+    ignores: ['dist/'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        ...globals.browser,
+        ...globals.es6,
+        window: true,
+      },
+    },
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptEslintParser,
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+    rules: {
+      'no-var': 2,
+      'no-unused-vars': 0,
+      '@typescript-eslint/no-unused-vars': [
+        2,
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+]
