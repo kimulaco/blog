237b14bc0abab697bc1ca5f172c6e2accb137bd8
kimulaco <11986753+kimulaco@users.noreply.github.com>
2024-11-07 00:51:54 +0900

    Update article packages version (#44)
    * Update microcms-js-sdk to v3.1
    * Update date-fns to v3
    * Update marked to v14

--- src/core/utilities/formatDate/index.ts
+++ src/core/utilities/formatDate/index.ts
@@ -1,9 +1,9 @@
 import { parseISO, format, isValid } from 'date-fns'
-import { utcToZonedTime } from 'date-fns-tz'
+import { toZonedTime } from 'date-fns-tz'
 import { BUILD_CONFIG } from '@@/config/build'
 
 export const formatDate = (value: string): string => {
-  const date = utcToZonedTime(parseISO(value), BUILD_CONFIG.TIMEZONE)
+  const date = toZonedTime(parseISO(value), BUILD_CONFIG.TIMEZONE)
 
   if (isValid(date)) {
     return format(date, 'yyyy-MM-dd')
