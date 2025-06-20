f6e3e774802b503315f422441513fb0befb77134
kimulaco <kimulaco@gmail.com>
2025-06-20 09:52:19 +0900

    Replace jsdom to happy-dom

--- test/utilities/parseHTML.ts
+++ test/utilities/parseHTML.ts
@@ -1,6 +1,7 @@
-import { JSDOM } from 'jsdom'
+import { Window } from 'happy-dom'
 
-export const parseHTML = (html: string): Document => {
-  const dom = new JSDOM(html)
-  return dom.window.document
+export const parseHTML = (html: string) => {
+  const window = new Window()
+  window.document.documentElement.innerHTML = html
+  return window.document
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { getViteConfig } from 'astro/config'
 export default getViteConfig({
   test: {
     globals: true,
-    environment: 'node',
+    environment: 'happy-dom',
     include: ['src/**/*.(test|spec).ts'],
     coverage: {
       all: true,
